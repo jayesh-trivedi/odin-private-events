5f21b88d42ce427bba3f9b7c012d3b6439a1d1a8
jayesh-trivedi <jayesh.trvdi@gmail.com>
2021-02-11 14:54:40 +0530

    added events model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_11_070513) do
+ActiveRecord::Schema.define(version: 2021_02_11_092307) do
+
+  create_table "events", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.date "date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"

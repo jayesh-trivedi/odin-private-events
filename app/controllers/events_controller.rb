066211c675be43d725df9f5a39e18e512eb4c609
jayesh-trivedi <jayesh.trvdi@gmail.com>
2021-02-11 15:20:33 +0530

    added events controller and associations with user

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,35 @@
+class EventsController < ApplicationController
+
+  before_action :user_signed_in?, only: [:new]
+
+  def index
+    @events = Event.all
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = User.find(session[:current_user_id]).created_events.build(event_params)
+    if @event.save
+      redirect_to user_path(session[:current_user_id])
+    else
+      render :new
+    end
+  end
+
+  def event_params
+    params.require(:event).permit(:title, :description, :date)
+  end
+
+  def user_signed_in?
+    unless User.exists?(id: session[:current_user_id])
+      redirect_to new_session_path, notice: "You need to sign in!"
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'events#index'
   resources :users, only: [:show, :new, :create]
+  resources :sessions, only: [:create, :new, :destroy]
+  resources :events, only: [:new, :create, :show, :index]
 end

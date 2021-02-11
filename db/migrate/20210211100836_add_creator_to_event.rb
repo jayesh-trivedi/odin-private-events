b0b1f7e14a231842bbac5c71d03a4d175334ec23
jayesh-trivedi <jayesh.trvdi@gmail.com>
2021-02-11 16:00:41 +0530

    added creator_id to event and updated views

---
+++ db/migrate/20210211100836_add_creator_to_event.rb
@@ -0,0 +1,7 @@
+class AddCreatorToEvent < ActiveRecord::Migration[6.1]
+  def change
+    add_reference :events, :creator, references: :users, index: true
+
+    add_foreign_key :events, :users, column: :creator_id
+  end
+end
